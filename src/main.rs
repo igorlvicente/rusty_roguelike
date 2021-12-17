a4e052f39ed808330cf842c6a6d752021c152a3b
Igor L. Vicente <igorlvicente@protonmail.com>
2021-12-16 22:12:05 -0300

    Adds map.rs and prelude to the main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use bracket_lib::prelude::*;
 
-const SCREEN_WIDTH: i32 = 80;
-const SCREEN_HEIGHT: i32 = 50;
+mod map;
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use prelude::*;
 
 struct State {}
 
