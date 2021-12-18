0c9589c12a4723986d9771249c438596104d798d
Igor L. Vicente <igorlvicente@protonmail.com>
2021-12-18 14:49:28 -0300

    Implements map.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 use bracket_lib::prelude::*;
 
 mod map;
+
 mod prelude {
     pub use bracket_lib::prelude::*;
+
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+
     pub use crate::map::*;
 }
 
 use prelude::*;
 
-struct State {}
+struct State {
+    map: Map,
+}
+
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, context: &mut BTerm) {
-        context.print_centered((SCREEN_HEIGHT as f32 / 2.0) as i32, "Hello, world!")
+        context.cls();
+        self.map.render(context);
     }
 }
 
@@ -22,7 +34,8 @@ fn main() -> BError {
     let context = BTermBuilder::simple(SCREEN_WIDTH, SCREEN_HEIGHT)
         .expect("Error when trying to create the screen")
         .with_title("Rusty Roguelike")
+        .with_fps_cap(30.0)
         .build()?;
 
-    main_loop(context, State {})
+    main_loop(context, State::new())
 }
