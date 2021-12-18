91f747c19e70f63fc31c469ba9f92fe24d408114
Igor L. Vicente <igorlvicente@protonmail.com>
2021-12-18 15:26:19 -0300

    Adds function to generate random rooms to map_builder.rs

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -7,3 +7,36 @@ pub struct MapBuilder {
     pub rooms: Vec<Rect>,
     pub player_start: Point,
 }
+
+impl MapBuilder {
+    fn fill(&mut self, tile: TileType) {
+        self.map.tiles.iter_mut().for_each(|map_tile| *map_tile = tile)
+    }
+
+    fn build_random_rooms(&mut self, random_number_generator: &mut RandomNumberGenerator) {
+        while self.rooms.len() < NUM_ROOMS {
+            let new_room = Rect::with_size(
+                random_number_generator.range(1, SCREEN_WIDTH - 10),
+                random_number_generator.range(1, SCREEN_HEIGHT - 10),
+                random_number_generator.range(2, 10),
+                random_number_generator.range(2, 10),
+            );
+            let mut overlap = false;
+            for room in self.rooms.iter() {
+                if room.intersect(&new_room) {
+                    overlap = true
+                }
+            }
+
+            if !overlap {
+                new_room.for_each(|point| {
+                    if point.x > 0 && point.x < SCREEN_WIDTH && point.y > 0 && point.y < SCREEN_HEIGHT {
+                        let index = map_index(point.x, point.y);
+                        self.map.tiles[index] = TileType::Floor;
+                    }
+                });
+                self.rooms.push(new_room)
+            }
+        }
+    }
+}
