233e43bc16d1fe1e1315663ed87f4ac0e759c52a
Igor L. Vicente <igorlvicente@protonmail.com>
2021-12-18 15:40:11 -0300

    Creates functions to build corridors in map_builder.rs

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -39,4 +39,42 @@ impl MapBuilder {
             }
         }
     }
+
+    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
+        use std::cmp::{min, max};
+
+        for y in min(y1, y2)..=max(y1, y2) {
+            if let Some(index) = self.map.try_index(Point::new(x, y)) {
+                self.map.tiles[index as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
+        use std::cmp::{min, max};
+
+        for x in min(x1, x2)..=max(x1, x2) {
+            if let Some(index) = self.map.try_index(Point::new(x, y)) {
+                self.map.tiles[index as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn build_corridors(&mut self, random_number_generator: &mut RandomNumberGenerator) {
+        let mut rooms = self.rooms.clone();
+        rooms.sort_by(|room_a, room_b| room_a.center().x.cmp(&room_b.center().x));
+
+        for (i, room) in rooms.iter().enumerate().skip(1) {
+            let previous_room = rooms[i-1].center();
+            let current_room = room.center();
+
+            if random_number_generator.range(0, 2) == 1 {
+                self.apply_horizontal_tunnel(previous_room.x, current_room.x, previous_room.y);
+                self.apply_vertical_tunnel(previous_room.y, current_room.y, current_room.x);
+            } else {
+                self.apply_horizontal_tunnel(previous_room.x, current_room.x, current_room.y);
+                self.apply_vertical_tunnel(previous_room.y, current_room.y, previous_room.x);
+            }
+        }
+    }
 }
